e7570efdd4af7cccd5cf34047efd2035bac8663f
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-10-07 09:29:43 +0200

    con constructores

---
+++ src/com/zubiri/geometria/Circulo.java
@@ -0,0 +1,28 @@
+package com.zubiri.geometria
+public class Circulo {
+	private double radio = 0;
+
+public Circulo(double radio){
+	this.radio = r
+	{
+public setRadio(double r){
+	this.radio = r;
+	{
+	
+ 	public double area (double r) {
+
+		double result = 0;
+		result = 3.1416*this.getRadio()*this.getRadio();
+		return result;
+	 }
+	public double circunf (double r) {
+
+		double result = 0;
+		result = 3.1416*2*this.getRadio();
+		return result;
+	 }
+
+	public double getRadio(){
+		return radio;
+	}
+}
